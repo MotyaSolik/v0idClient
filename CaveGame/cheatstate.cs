3105ab1fde98ca9c7c9c0c52cfcda0b30603608c
MotyaSolik <motya.solik@gmail.com>
2026-05-03 00:12:08 +0300

    fix CaveGame submodule

---
+++ CaveGame/cheatstate.cs
@@ -0,0 +1,9 @@
+﻿namespace CaveGame
+{
+    public static class CheatState
+    {
+        public static bool Noclip = false;
+        public static bool Godmode = false;
+        public static bool Vision = false;
+    }
+}
